e77a509ed7b082de906af998a6983c62081b9175
KazancevaYuliya <87359439+KazancevaYuliya@users.noreply.github.com>
2022-01-16 12:59:51 +0300

    Add files via upload

---
+++ ДЗ/script.js
@@ -0,0 +1,36 @@
+// ---1.---
+
+//const buttonElement = document.querySelector('button')
+
+//let counter = 0
+    //function handleClickButton() {
+        //if (counter < 10) {
+            //counter++
+    //} else {
+        //buttonElement.style.background = 'red'
+    //}
+//}
+//buttonElement.addEventListener('click', handleClickButton )
+
+// ---2.---
+
+//function myFunction(menu) {
+    //menu.classList.toggle("change");
+  //}
+    
+ 
+    
+// ---3.---
+
+//let modal = document.getElementById('myModal')
+//let button1 = document.getElementById('button1')
+//let span = document.getElementsByClassName('close')[0]
+   
+//button1.onclick = function () {
+    //modal.style.display = 'block'
+    
+//}
+//span.onclick = function () {
+    //modal.style.display = 'none'
+    
+//}
